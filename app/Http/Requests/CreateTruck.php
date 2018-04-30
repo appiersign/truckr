edc64a8025968a62e194e9950db19352aa684ecb
Solomon Appier-Sign <sappiah@payswitch.com.gh>
2018-04-30 10:33:00 +0000

    trucker model and migration created

--- app/Http/Requests/CreateTruck.php
+++ app/Http/Requests/CreateTruck.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class CreateTruck extends FormRequest
 {
@@ -13,7 +14,7 @@ class CreateTruck extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return Auth::user()->type === 'owner';
     }
 
     /**
@@ -24,7 +25,11 @@ class CreateTruck extends FormRequest
     public function rules()
     {
         return [
-            //
+            'license_plate' => 'required|min:5',
+            'model' => 'required|min:3',
+            'make' => 'required|min:3',
+            'type' => 'required',
+            'capacity' => 'required'
         ];
     }
 }
---
+++ database/migrations/2018_04_30_103204_create_truckers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTruckersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('truckers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('truckers');
+    }
+}
