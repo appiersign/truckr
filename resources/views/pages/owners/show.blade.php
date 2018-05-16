536448a2844d2ff20b5fb1a54b7457690e8e5d50
Amaka Nwosu <nwosuchiamaka@gmail.com>
2018-05-16 10:59:04 +0000

    adds basic structures to files in the pages directory

---
+++ resources/views/pages/owners/show.blade.php
@@ -0,0 +1,11 @@
+<!-- we have to inherit or extend the dashboard layout -->
+@extends('layouts.dashboard')
+<!-- the page title -->
+@section('title')
+  Display Truck Owner Details
+@endsection
+
+<!-- we then tell which section or part of the layout we want to push the view into -->
+@section('content')
+
+@endsection
